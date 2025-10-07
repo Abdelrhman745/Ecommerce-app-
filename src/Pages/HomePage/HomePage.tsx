dbf1c51b3f8d1c757387a474bde746c155bb5e19
Rahmaabdelrazeq-dev <raahhma55@gmail.com>
2025-10-07 23:47:26 +0300

    HomePage Updates Sections

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import About from '../../components/Aboutcomponent/Aboutcomponent'
-import Categories from '../../components/Categories/Categories'
-import TextSec from '../../components/TextSection/TextSection'
-import Gallery from '../../components/Gallery/Gallery'
+import React from "react";
+import About from "../../components/Aboutcomponent/Aboutcomponent";
+import Categories from "../../components/Categories/Categories";
+import TextSec from "../../components/TextSection/TextSection";
+import Gallery from "../../components/Gallery/Gallery";
+import CarouselSection from "../../components/CarouselSection/CarouselSection";
+import ProductBlockSection from "../../components/ProductBlockSection/ProductBlockSection";
 
 export default function HomePage() {
   return (
     <>
-      <About/>
-      <Categories/>
-      <TextSec/>
-      <Gallery/>
+      <CarouselSection />
+      <ProductBlockSection />
+      <About />
+      <Categories />
+      <TextSec />
+      <Gallery />
     </>
-  )
+  );
 }
