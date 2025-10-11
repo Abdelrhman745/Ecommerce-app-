cf3b7ff242f3ea98578f5fd8ffcb3d80c2d398a1
Rahmaabdelrazeq-dev <raahhma55@gmail.com>
2025-10-11 16:57:12 +0300

    Edit Carousel

--- src/components/CarouselSection/CarouselSection.tsx
+++ src/components/CarouselSection/CarouselSection.tsx
@@ -7,8 +7,8 @@ import "swiper/css/effect-fade";
 import "swiper/css/pagination";
 
 const slides = [
-  { title: "Elegant Skincare", image: "/1.jpg" },
-  { title: "Premium Creams", image: "/2.jpg" },
+  { title: "Elegant Skincare", image: "/2.jpg" },
+  { title: "Premium Creams", image: "/1.jpg" },
   { title: "Natural Radiance", image: "/3.jpg" },
 ];
 
