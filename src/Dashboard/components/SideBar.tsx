802258cd5100845a04805d70f3fb76dc99119ce2
Ananhamdy11 <ananhamdy1911@gmail.com>
2025-10-22 16:04:48 +0300

    update responsive

--- src/Dashboard/components/SideBar.tsx
+++ src/Dashboard/components/SideBar.tsx
@@ -1,49 +1,142 @@
+// import React from "react";
+// import { Nav, Card } from "react-bootstrap";
+// import { Link, useLocation } from "react-router-dom";
+
+
+// export default function SideBar() {
+//   const location = useLocation();
+
+
+//    const menuItems = [
+//   { Label: "Dashboard", icon: "bi-speedometer2", path: "/dashboard" },
+//     { Label: "Products", icon: "bi-tags-fill", path: "/dashboard/products" },
+//       { Label: "Orders", icon: "bi-bag-fill", path: "/dashboard/orders" },
+//   { Label: "Users", icon: "bi-person-fill", path: "/dashboard/user" },
+//   { Label: "Messages", icon: "bi-chat-dots-fill", path: "/dashboard/contact" },
+//   { Label: "Reports", icon: "bi-file-earmark-bar-graph-fill", path: "/dashboard/reports" },
+//   { Label: "Logout", icon: "bi-box-arrow-right", path: "/login" }
+// ];
+
+
+//   return (
+//     <div
+//       className="d-flex flex-column"
+//       style={{
+//         width: "260px",
+//         minHeight: "100vh",
+//         backgroundColor: "#483B32",
+//         padding: "20px 15px",
+//         position: "fixed",
+// top: 0,
+// left: 0,
+// height: "auto",
+
+//       }}
+//     >
+//         <style>
+//     {`
+//       @media (max-width: 768px) {
+//         div.d-flex.flex-column {
+//           position: relative !important;
+//           width: 100% !important;
+//           height: auto !important;
+//         }
+//       }
+//     `}
+//   </style>
+//   <h3
+//   className="text-white text-center mb-3"
+//   style={{
+//     padding: "10px 0",
+//     fontFamily: "'Playfair Display', serif",
+//     fontWeight: "700",
+//     fontSize: "24px",
+//     letterSpacing: "1px",
+//   }}
+// >
+//   GlowUp
+// </h3>
+
+
+//       <Card
+//         className="text-center mb-4"
+//         style={{
+//           backgroundColor: "#BBA591",
+//           color: "#fff",
+//         }}
+//       >
+//         <Card.Body>
+//           <Card.Title className="fs-5">👤 Admin</Card.Title>
+//           <Card.Text style={{ fontSize: "0.9rem" }}>admin@gmail.com</Card.Text>
+//         </Card.Body>
+//       </Card>
+
+//       <Nav className="flex-column gap-2 mb-1">
+//         {menuItems.map((item) => (
+//           <Nav.Link
+//             as={Link}
+//             to={item.path}
+//             key={item.path}
+//             className={`d-flex align-items-center fs-6 fw-normal  rounded`} 
+//             style={{
+//               color:"#fff",
+//     backgroundColor:
+//       location.pathname === item.path ? "#726255" : "transparent",
+//     transition: "all 0.2s",
+//   }}
+//           >
+//             <span
+//               style={{
+//                 marginRight: "15px",
+//                 fontSize: "20px",
+//                 display: "flex",
+//                 alignItems: "center",
+//               }}
+//             >
+//     <i className={`bi ${item.icon}`}></i>
+//             </span>
+//             {item.Label}
+//           </Nav.Link>
+//         ))}
+//       </Nav>
+       
+//     </div>
+//   );
+// }
+
+
 import React from "react";
 import { Nav, Card } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 
-
 export default function SideBar() {
   const location = useLocation();
 
-
-   const menuItems = [
-  { Label: "Dashboard", icon: "bi-speedometer2", path: "/dashboard" },
+  const menuItems = [
+    { Label: "Dashboard", icon: "bi-speedometer2", path: "/dashboard" },
     { Label: "Products", icon: "bi-tags-fill", path: "/dashboard/products" },
-      { Label: "Orders", icon: "bi-bag-fill", path: "/dashboard/orders" },
-  { Label: "Users", icon: "bi-person-fill", path: "/dashboard/user" },
-  { Label: "Messages", icon: "bi-chat-dots-fill", path: "/dashboard/contact" },
-  { Label: "Reports", icon: "bi-file-earmark-bar-graph-fill", path: "/dashboard/reports" },
-  { Label: "Logout", icon: "bi-box-arrow-right", path: "/login" }
-];
-
+    { Label: "Orders", icon: "bi-bag-fill", path: "/dashboard/orders" },
+    { Label: "Users", icon: "bi-person-fill", path: "/dashboard/user" },
+    { Label: "Messages", icon: "bi-chat-dots-fill", path: "/dashboard/contact" },
+    { Label: "Reports", icon: "bi-file-earmark-bar-graph-fill", path: "/dashboard/reports" },
+    { Label: "Logout", icon: "bi-box-arrow-right", path: "/login" },
+  ];
 
   return (
-    <div
-      className="d-flex flex-column"
-      style={{
-        width: "250px",
-        minHeight: "100vh",
-        backgroundColor: "#483B32",
-        padding: "20px 15px",
-      }}
-    >
-  <h3
-  className="text-white text-center mb-3"
-  style={{
-    padding: "10px 0",
-    fontFamily: "'Playfair Display', serif",
-    fontWeight: "700",
-    fontSize: "1.8rem",
-    letterSpacing: "1px",
-  }}
->
-  GlowUp
-</h3>
+    <div className="sidebar d-flex flex-column">
+      <h3 className="text-white text-center mb-2"
+        style={{
+          padding: "10px 0",
+          fontFamily: "'Playfair Display', serif",
+          fontWeight: "700",
+          fontSize: "24px",
+          letterSpacing: "1px",
+        }}
+      >
+        GlowUp
+      </h3>
 
-
-      <Card
-        className="text-center mb-4"
+      <Card className="text-center mb-4"
         style={{
           backgroundColor: "#BBA591",
           color: "#fff",
@@ -55,35 +148,62 @@ export default function SideBar() {
         </Card.Body>
       </Card>
 
-      <Nav className="flex-column gap-2 mb-3">
+      <Nav className="flex-column gap-2 mb-1">
         {menuItems.map((item) => (
           <Nav.Link
             as={Link}
             to={item.path}
             key={item.path}
-            className={`d-flex align-items-center fs-5 fw-normal py-3 px-3 rounded`} 
+            className={`d-flex align-items-center fs-6 fw-normal rounded mb-1`}
             style={{
-              color:"#fff",
-    backgroundColor:
-      location.pathname === item.path ? "#726255" : "transparent",
-    transition: "all 0.2s",
-  }}
+              color: "#fff",
+              backgroundColor:
+                location.pathname === item.path ? "#726255" : "transparent",
+              transition: "all 0.2s",
+            
+            }}
           >
             <span
               style={{
                 marginRight: "15px",
-                fontSize: "1.5rem",
+                fontSize: "20px",
                 display: "flex",
                 alignItems: "center",
               }}
             >
-    <i className={`bi ${item.icon}`}></i>
+              <i className={`bi ${item.icon}`}></i>
             </span>
             {item.Label}
           </Nav.Link>
         ))}
       </Nav>
-       
+
+      <style>
+        {`
+          @media (max-width: 768px) {
+            .sidebar {
+              position: relative !important;
+              width: 100% !important;
+              height: auto !important;
+              min-height: auto !important;
+                            background-color: #483B32;
+
+            }
+          }
+
+          @media (min-width: 769px) {
+            .sidebar {
+              position: fixed;
+              top: 0;
+              left: 0;
+              width: 260px;
+              height: 100vh;
+              background-color: #483B32;
+              padding: 20px 15px;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 }
