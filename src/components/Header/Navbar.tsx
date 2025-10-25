d46f34c681f06609f19b550a1747f38246b97f96
Abdelrhman745 <abdelrhmanibrahimkamal@gmail.com>
2025-10-25 15:42:03 +0300

    Responsive in Navbar

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -96,7 +96,19 @@ const Navbar: React.FC = () => {
           </button>
 
           {/* Navbar Links */}
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div
+            className="collapse navbar-collapse"
+            id="navbarNav"
+            onClick={() => {
+              const nav = document.getElementById("navbarNav");
+              if (nav?.classList.contains("show")) {
+                const toggleButton = document.querySelector(
+                  ".navbar-toggler"
+                ) as HTMLElement;
+                toggleButton?.click();
+              }
+            }}
+          >
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-3">
               {navLinks.map((item, idx) => (
                 <li key={idx} className="nav-item">
