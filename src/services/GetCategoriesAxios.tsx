7d958b4e2c4a747aae8c74098392cb386a75783c
Abdelrhman745 <abdelrhmanibrahimkamal@gmail.com>
2025-12-07 20:46:13 +0200

    final

--- src/services/GetCategoriesAxios.tsx
+++ src/services/GetCategoriesAxios.tsx
@@ -1,3 +1,4 @@
+// src/services/GetCategoriesAxios.tsx
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -9,13 +10,20 @@ interface Product {
 }
 
 export async function fetchProducts(): Promise<string[]> {
+  // جلب البيانات من الـ API
   const response = await axios.get<{ data: Product[] }>(
-  "https://68f278b4b36f9750deecbed2.mockapi.io/data/api/products"
-);
-const allBrands = response.data.map((item: Product) => item.category);
-const uniqueBrands = [...new Set(allBrands)];
+    "https://68f278b4b36f9750deecbed2.mockapi.io/data/api/products"
+  );
+
+  // استخراج الفئات من response.data.data
+  const allBrands = response.data.data.map((item: Product) => item.category);
+
+  // إزالة التكرارات
+  const uniqueBrands = [...new Set(allBrands)];
+
   return uniqueBrands;
 }
+
 export function useProductsQuery() {
   return useQuery<string[]>({
     queryKey: ["categories"],
